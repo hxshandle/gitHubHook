021e2b5cf6e7688a980c8beb7ab0304658dadd4a
hxshandle <handle.huang0409@gmail.com>
2014-02-06 21:02:12 +0800

    first code

---
+++ routes/gitHubHook.js
@@ -0,0 +1,30 @@
+/**
+ * Created by hhuang on 2/6/14.
+ */
+
+
+var exec = require('child_process').exec,
+    path = require('path'),
+    fs = require('fs');
+exports.gitHubHook = function (req, res) {
+  var postData = req.param('payload', 'null');
+  var data = JSON.parse(postData);
+  var repoUrl = data.repository.url.split("/");
+  var repoName = repoUrl[repoUrl.length - 1];
+  var f = path.join(__dirname, '../config/repos.json');
+  fs.readFile(f, {encoding: 'utf8'}, function (err, data) {
+    if (err) throw err;
+    var cfg = JSON.parse(data);
+    console.log('config data :' + data);
+    console.log('working dir :' +cfg[repoName]['optDir']);
+    exec('git status', {cwd: cfg[repoName]['optDir']}, function (error, stdout, stderr) {
+      console.log('stderr: ' + stderr);
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+    });
+  });
+  console.log("dirname: " + __dirname);
+
+  res.render('gitHubHook', { title: 'Hook', pd: repoName });
+};
